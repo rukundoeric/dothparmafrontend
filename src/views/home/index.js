93cb1e9708303d6e31108e9a0296f173620dab4f
Rukundo Eric <ericrukundo005@gmail.com>
2022-04-07 22:24:09 +0200

    setup linters

---
+++ src/views/home/index.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import logo from '../../logo.svg';
+
+export default function index() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Welcome to DotPharma!
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a>
+    </header>
+  </div>
+  )
+}
